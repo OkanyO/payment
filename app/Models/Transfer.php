40b4150c86ba1e6998a54fd695c0d8874fcf5db1
OkanyO <Okanynuel@gmail.com>
2024-07-17 15:56:14 -0700

    Payment api with user transaction history

---
+++ app/Models/Transfer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transfer extends Model
+
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo('sender_id');
+
+    }
+  public function senderAccount(): BelongsTo
+  {
+    return $this->belongsTo(Account::class,'sender_account_id');
+  }
+
+    public function recipient(): BelongsTo
+    {
+        return $this->belongsTo('recipient_id');
+
+    }
+
+    public function recipientAccount(): BelongsTo
+    {
+        return $this->belongsTo(Account::class,'recipient_account_id');
+    }
+}
